251c3943bfad181643054c828bdf0e3d8f94b22b
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-13 12:10:23 +0300

    finish the step 2 - Runner with a Fixed Sport

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ class Swimmer(Athlete):
 
 tom = Swimmer("Tom", 22)
 tom.introduce()
+
+# step 2 - Runner with a Fixed Sport
+class Athlete:
+    def __init__(self, name, age, sport):
+        self.name = name
+        self.age = age
+        self.sport = sport
+    def describe(self):
+        print(f"{self.name} competes in {self.sport}.")
+class Runner(Athlete):
+    def __init__(self, name, age):
+        super().__init__(name, age, "Running")
+sara = Runner("Sara", 25)
+sara.describe()
